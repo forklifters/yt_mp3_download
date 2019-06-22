139fb30aa35152452a471190bce1ab69e54efc91
Philipp <p.koenig@blockbyte.de>
2019-06-22 21:28:35 +0200

    Moved executables into bin folder
    Signed-off-by: Philipp <p.koenig@blockbyte.de>

--- download.ps1
+++ download.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = "Stop"
 
 function GetMetaDataFromURL ($url)
 {
-    $originalVideoTitle = ./youtube-dl.exe --no-playlist --get-filename -o "%(title)s" $url
+    $originalVideoTitle = ./bin/youtube-dl.exe --no-playlist --get-filename -o "%(title)s" $url
 
     $videoTitle = $originalVideoTitle
     $videoTitle = $videoTitle -ireplace  "(\s|\()feat\.",'$1ft.' # 'feat. xy' -> 'ft. xy'
@@ -77,7 +77,7 @@ function DownloadAudio ($url)
     }
 
 
-    ./youtube-dl.exe --no-playlist --format bestaudio --extract-audio --audio-format mp3 --audio-quality 0 -o "./output/__tmp.%(ext)s" $url
+    ./bin/youtube-dl.exe --no-playlist --format bestaudio --extract-audio --audio-format mp3 --audio-quality 0 -o "./output/__tmp.%(ext)s" $url
 }
 
 
@@ -86,7 +86,7 @@ function SaveAudioWithMetaData ($metaData)
     $destFilename = "./output/$($metaData['artist']) - $($metaData['title']).mp3"
     Remove-Item $destFilename -ErrorAction Ignore
 
-    ./ffmpeg.exe -i "./output/__tmp.mp3" -c copy -metadata artist="$($metaData['artist'])" -metadata title="$($metaData['title'])" $destFilename
+    ./bin/ffmpeg.exe -i "./output/__tmp.mp3" -c copy -metadata artist="$($metaData['artist'])" -metadata title="$($metaData['title'])" $destFilename
 
     Remove-Item "./output/__tmp.mp3" -ErrorAction Ignore
     Write-Host "`nSaved file as $destFilename`n"
--- update.ps1
+++ update.ps1
@@ -6,17 +6,24 @@ $ErrorActionPreference = "Stop"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 
+if (![System.IO.Directory]::Exists("./bin"))
+{
+    New-Item -ItemType Directory -Force -Path "./bin"
+}
+
+
 # Download the latest version of ffmpeg and extract to executables from the zip file
 Write-Host "Fetching latest version of ffmpeg ..."
-Invoke-WebRequest https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip -OutFile ffmpeg.zip
+Invoke-WebRequest https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip -OutFile bin/ffmpeg.zip
 
-$zip = [IO.Compression.ZipFile]::OpenRead("ffmpeg.zip")
-$zip.Entries | where {$_.Name -like '*.exe'} | foreach {[System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "./" + $_.Name, $true)}
+Add-Type -Assembly System.IO.Compression.FileSystem
+$zip = [IO.Compression.ZipFile]::OpenRead("bin/ffmpeg.zip")
+$zip.Entries | where {$_.Name -like '*.exe'} | foreach {[System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "bin/" + $_.Name, $true)}
 $zip.Dispose()
 
-Remove-Item ffmpeg.zip -ErrorAction Ignore
+Remove-Item ./bin/ffmpeg.zip -ErrorAction Ignore
 
 
 # Download the latest executable of youtube-dl
 Write-Host "Fetching latest version of youtube-dl ..."
-Invoke-WebRequest https://yt-dl.org/latest/youtube-dl.exe -OutFile youtube-dl.exe
+Invoke-WebRequest https://yt-dl.org/latest/youtube-dl.exe -OutFile bin/youtube-dl.exe
