daeb13b5dfb34e6734416a516403639dcef1f30c
Philipp <p.koenig@blockbyte.de>
2019-07-14 11:02:09 +0200

    Improved dialog with status messages and Console-Clears
    Signed-off-by: Philipp <p.koenig@blockbyte.de>

--- update.ps1
+++ update.ps1
@@ -1,10 +1,17 @@
 #Requires -Version 5.1
 
 Set-StrictMode -Version Latest
-$ErrorActionPreference = "Stop"
+Remove-Variable * -ErrorAction SilentlyContinue; Remove-Module *; $error.Clear(); Clear-Host
+
+Import-Module ./modules/inputOutput.psm1
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
+
+#
+#
+# Download the latest version of ffmpeg and extract to executables from the zip file
+#
 
 if (![System.IO.Directory]::Exists("./bin"))
 {
@@ -12,10 +19,18 @@ if (![System.IO.Directory]::Exists("./bin"))
 }
 
 
+#
+#
+#
 # Download the latest version of ffmpeg and extract to executables from the zip file
-Write-Host "Fetching latest version of ffmpeg ..."
+#
+
+clearConsole
+ShowText "Downloading latest version of ffmpeg ..." -fc DarkCyan
 Invoke-WebRequest https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip -OutFile bin/ffmpeg.zip
 
+clearConsole
+ShowText "Unpacking ffmpeg ..." -fc DarkCyan
 Add-Type -Assembly System.IO.Compression.FileSystem
 $zip = [IO.Compression.ZipFile]::OpenRead("bin/ffmpeg.zip")
 $zip.Entries | where {$_.Name -like '*.exe'} | foreach {[System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "bin/" + $_.Name, $true)}
@@ -24,6 +39,12 @@ $zip.Dispose()
 Remove-Item ./bin/ffmpeg.zip -ErrorAction Ignore
 
 
+#
+#
+#
 # Download the latest executable of youtube-dl
-Write-Host "Fetching latest version of youtube-dl ..."
+#
+
+clearConsole
+ShowText "Fetching latest version of youtube-dl ..." -fc DarkCyan
 Invoke-WebRequest https://yt-dl.org/latest/youtube-dl.exe -OutFile bin/youtube-dl.exe
